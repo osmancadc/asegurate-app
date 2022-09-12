76c523e5cc48aad97fa1360b6f7f2511833ec6e8
yderfre78 <72670647+yderfre78@users.noreply.github.com>
2022-09-12 13:27:46 -0500

    creatring_widget_menu_drawer

---
+++ lib/widgets/menu_drawer.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MenuDrawer extends GetxController {
+  Widget menuDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text('Drawer Header'),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          ListTile(
+            title: Text('Item 1'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title: Text('Item 2'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'package:app_asegurate/widgets/menu_drawer.dart';
